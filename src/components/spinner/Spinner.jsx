db78e77823176964c242ab21f5ea676b4478313e
Iryna Artymuk <artimyk@ukr.net>
2024-02-11 14:52:25 +0200

    add custom color to spinner

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -2,11 +2,12 @@ import BarLoader from 'react-spinners/BarLoader';
 const override = {
   display: 'block',
   margin: '0 auto',
-  borderColor: '#459DE9',
+  borderColor: '#FFA25B',
 };
 const Spinner = () => {
   return (
     <BarLoader
+      color={'#FFA25B'}
       cssOverride={override}
       size={150}
       aria-label="Loading Spinner"
